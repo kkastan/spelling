d2d2fa4535f7e575c894d494c9e9879f5525e701
Karl Kastan <karlkastan@gmail.com>
2014-06-30 13:52:52 -0700

    adding audio

--- src/main/java/net/kastan/spelling/SpellingActivity.java
+++ src/main/java/net/kastan/spelling/SpellingActivity.java
@@ -1,6 +1,9 @@
 package net.kastan.spelling;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -10,6 +13,9 @@ import android.widget.TextView;
 import net.kastan.spelling.engine.Game;
 import net.kastan.spelling.engine.GameState;
 import net.kastan.spelling.util.Constants;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SpellingActivity extends Activity {
@@ -21,6 +27,53 @@ public class SpellingActivity extends Activity {
   private Game game;
   private TextView[] displayChars;
   private TextView[] choiceChars;
+
+  private boolean isVisible = false;
+
+  private static Map<Character, Integer> soundsByCharMap = new HashMap<Character, Integer>();
+  static {
+    soundsByCharMap.put('a', R.raw.a);
+    soundsByCharMap.put('b', R.raw.b);
+    soundsByCharMap.put('c', R.raw.c);
+    soundsByCharMap.put('d', R.raw.d);
+    soundsByCharMap.put('e', R.raw.e);
+    soundsByCharMap.put('f', R.raw.f);
+    soundsByCharMap.put('g', R.raw.g);
+    soundsByCharMap.put('h', R.raw.h);
+    soundsByCharMap.put('i', R.raw.i);
+    soundsByCharMap.put('j', R.raw.j);
+    soundsByCharMap.put('k', R.raw.k);
+    soundsByCharMap.put('l', R.raw.l);
+    soundsByCharMap.put('m', R.raw.m);
+    soundsByCharMap.put('n', R.raw.n);
+    soundsByCharMap.put('o', R.raw.o);
+    soundsByCharMap.put('p', R.raw.p);
+    soundsByCharMap.put('q', R.raw.q);
+    soundsByCharMap.put('r', R.raw.r);
+    soundsByCharMap.put('s', R.raw.s);
+    soundsByCharMap.put('t', R.raw.t);
+    soundsByCharMap.put('u', R.raw.u);
+    soundsByCharMap.put('v', R.raw.v);
+    soundsByCharMap.put('w', R.raw.w);
+    soundsByCharMap.put('x', R.raw.x);
+    soundsByCharMap.put('y', R.raw.y);
+    soundsByCharMap.put('z', R.raw.z);
+  }
+
+
+  private MediaPlayer mediaPlayer;
+
+
+  private AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+
+    }
+  };
+
+  /**
+   * Android Activity Life Cycle Methods
+   */
 
   /** Called when the activity is first created. */
   @Override
@@ -51,6 +104,48 @@ public class SpellingActivity extends Activity {
     startNextGame();
   }
 
+  @Override
+  protected void onResume() {
+    super.onResume();
+    isVisible = true;
+
+    setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+    AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    audioManager.requestAudioFocus(afChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    isVisible = false;
+
+    if(null != mediaPlayer) {
+      mediaPlayer.release();
+      mediaPlayer = null;
+    }
+
+    AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    audioManager.abandonAudioFocus(afChangeListener);
+  }
+
+
+  /**
+   * Spelling Activity related methods
+   */
+
+
+  public void sayLetter(char c) {
+    if(null != mediaPlayer) {
+      mediaPlayer.release();
+      mediaPlayer = null;
+    }
+
+    if(isVisible && soundsByCharMap.containsKey(c)) {
+      mediaPlayer = MediaPlayer.create(getApplicationContext(), soundsByCharMap.get(c));
+      mediaPlayer.start();
+    }
+  }
 
   public void startNextGame() {
     game = new Game(gameWords[iteration], gameImageIds[iteration]);
@@ -78,6 +173,8 @@ public class SpellingActivity extends Activity {
   private void handleChoiceSelection(int index) {
     if(GameState.CHOOSING == game.getState()) {
       char selectedChar = choiceChars[index].getText().charAt(0);
+      sayLetter(selectedChar);
+
       if(game.isSelectionCorrect(selectedChar)) {
 
         game.setState(GameState.SHOWING_RESULTS);
@@ -154,6 +251,7 @@ public class SpellingActivity extends Activity {
 
   /**
    *
+   *
    */
   private static class HighlightDisplayCharsHandler extends Handler {
     int index = 0;
@@ -181,6 +279,8 @@ public class SpellingActivity extends Activity {
 
       if(index < 3) {
         chars[index].setTextColor(Constants.HIGHLIGHT_TEXT_COLOR);
+        char c = chars[index].getText().charAt(0);
+        parent.sayLetter(c);
       }
 
       index++;
